c3e26b5869a2be363e74a5a972cf45c0c5a6fd04
stanislavCasciuc <stas@DESKTOP-7VG3N5N>
2025-03-31 12:37:35 +0300

    Add project files.

---
+++ Rezervari-Ui/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RestaurantApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<Restaurant> restaurants = FetchRestaurantData();
+            dataGridView1.DataSource = restaurants;
+        }
+
+        private List<Restaurant> FetchRestaurantData()
+        {
+           
+            return new List<Restaurant>
+            {
+                new Restaurant { Name = "Restaurant A", Address = "123 Main St", Rating = 4.5 },
+                new Restaurant { Name = "Restaurant B", Address = "456 Elm St", Rating = 4.0 },
+                new Restaurant { Name = "Restaurant C", Address = "789 Oak St", Rating = 4.2 },
+            };
+        }
+    }
+
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ Rezervari-Ui/Program.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezervari_Ui
+{
+    static class Program
+    {
+      
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
